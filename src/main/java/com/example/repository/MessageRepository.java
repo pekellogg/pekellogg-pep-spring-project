2216b3b81991e69410ce7d55d63c3d49522c9206
Peyton Kellogg <peytonkellogg@gmail.com>
2024-01-16 23:05:34 +0000

    Imported deps for MessageRepository extends JpaRepository

--- src/main/java/com/example/repository/MessageRepository.java
+++ src/main/java/com/example/repository/MessageRepository.java
@@ -1,4 +1,9 @@
 package com.example.repository;
+import com.example.entity.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MessageRepository {
+/**
+ * JPA Repository interface for the Message entity
+ */
+public interface MessageRepository extends JpaRepository<Message, Long> {
 }
