7da97281c0ee67d904d246c947db0d558d2f33c4
Peyton Kellogg <peytonkellogg@gmail.com>
2024-01-17 02:00:26 +0000

    Imported Account & Message entities and Spring Annotations; added @RestController to SocialMediaController

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,12 +1,16 @@
 package com.example.controller;
 
+import com.example.entity.Account;
+import com.example.entity.Message;
+import org.springframework.web.bind.annotation.*;
 
 /**
- * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
+ * TO DO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
  * found in readme.md as well as the test cases. You be required to use the @GET/POST/PUT/DELETE/etc Mapping annotations
  * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
+@RestController
 public class SocialMediaController {
 
 }
