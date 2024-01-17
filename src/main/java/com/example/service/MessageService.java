ae627d65648519961fadbb06c612782fc2352e4f
Peyton Kellogg <peytonkellogg@gmail.com>
2024-01-17 02:43:10 +0000

    Imported and used Spring's @Transactional annotation for AccountService and MessageService

--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -4,10 +4,12 @@ import com.example.entity.Message;
 import com.example.repository.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@Transactional
 public class MessageService {
     MessageRepository messageRepository;
 
