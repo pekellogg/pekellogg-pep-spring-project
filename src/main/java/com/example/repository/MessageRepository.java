323c257f331dd056ebd98df8dfa84a58408f2b46
Peyton Kellogg <peytonkellogg@gmail.com>
2024-01-19 06:42:05 +0000

    Added findX methods in MessageRepository; added corresponding MessageService methods

--- src/main/java/com/example/repository/MessageRepository.java
+++ src/main/java/com/example/repository/MessageRepository.java
@@ -1,37 +1,14 @@
 package com.example.repository;
 import com.example.entity.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-/**
- * NamedQueries are a powerful feature of JPA repositorties that allow a developer to create a query using naming
- * conventions based on the name of fields in a class and the class name itself.
-
- * There are two important notes to keep in mind here. The return type and name of the method is important when
- * using named queries. For the first method, we are locating a single person object, so the name is "FindPerson..."
- * and the return type matches this expectation by returning a single Person object. We are locating this person
- * using the field "Name", so the full method name: "findPersonByName" follows a naming convention that JpaRepository
- * expects.
-
- * For the second method, notice that the proper way to say we want to find multiple persons would be to name
- * the method "FindPeopleByAge", however, the class is 'Person' not 'People'. The most important thing to keep in
- * mind when working with named queries are to match the class and field names.
- * **/
-
-// Account class fields:
-//     account_id int primary key auto_increment,
-//     username varchar(255) not null unique,
-//     password varchar(255)
-// }
-
-// Message class fields:
-//     message_id int primary key auto_increment,
-//     posted_by int,
-//     message_text varchar(255),
-//     time_posted_epoch bigint,
-//     foreign key (posted_by) references account(account_id)
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import java.util.List;
 
 /**
  * JPA Repository interface for the Message entity
  */
-public interface MessageRepository extends JpaRepository<Message, Long> {
+public interface MessageRepository extends JpaRepository<Message, Integer> {
+    @Query("FROM Message WHERE posted_by = :account_id")
+    List<Message> findAllByPostedBy(@Param("account_id") int accountId);
 }
--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -1,28 +1,29 @@
 package com.example.service;
-
 import com.example.entity.Message;
 import com.example.repository.MessageRepository;
+import com.example.repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.dao.EmptyResultDataAccessException;
 import java.util.List;
 import java.util.Optional;
 
 @Service
-@Transactional
 public class MessageService {
     MessageRepository messageRepository;
+    AccountRepository accountRepository;
 
     @Autowired
-    public MessageService(MessageRepository messageRepository) {
+    public MessageService(MessageRepository messageRepository, AccountRepository accountRepository) {
         this.messageRepository = messageRepository;
+        this.accountRepository = accountRepository;
     }
 
     /**
      * @param transient message
      * @return persisted message
      */
-    public Message persistMessage(Message message) {
+    public Message save(Message message) {
         return messageRepository.save(message);
     }
 
@@ -34,10 +35,17 @@ public class MessageService {
     }
 
     /**
+     * @return all message entities by posted_by
+     */
+    public List<Message> getAllMessagesByAccountId(int accountId) {
+        return messageRepository.findAllByPostedBy(accountId);
+    }
+
+    /**
      * @param message entity's id
      * @return message entity
      */
-    public Message getMessageById(long id) {
+    public Message getMessageById(int id) {
         Optional<Message> optionalMessage = messageRepository.findById(id);
 
         if (optionalMessage.isPresent()) {
@@ -47,8 +55,13 @@ public class MessageService {
         }
     }
 
-    public void deleteMessage(long id) {
-        messageRepository.deleteById(id);
+    public int deleteMessage(int id) {
+        try {
+            messageRepository.deleteById(id);
+            return 1;
+        } catch (EmptyResultDataAccessException e) {
+            return 0;
+        }
     }
 
     /**
@@ -56,13 +69,13 @@ public class MessageService {
      * @param replacement: message entity with updated field values
      * @return the updated message entity
      */
-    public Message updateMessage(long id, Message replacement) {
+    public Message updateText(int id, String text) {
         // findById returns a type Optional<Message>. This helps the developer avoid null pointer
         // exceptions. We can use the method .get() to convert an Optional<Message> to Message.
         Optional<Message> optionalMessage = messageRepository.findById(id);
         if (optionalMessage.isPresent()) {
             Message message = optionalMessage.get();
-            message.setMessage_text(replacement.getMessage_text());
+            message.setMessage_text(text);
             messageRepository.save(message);
             return message;
         } else {
