9eedd23769a9f87825f4cfed134422f6c21350f3
Peyton Kellogg <peytonkellogg@gmail.com>
2024-01-19 06:41:09 +0000

    Added findX methods in AccountRepository; added corresponding AccountService methods

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -4,12 +4,9 @@ import com.example.entity.Account;
 import com.example.repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import java.util.List;
 import java.util.Optional;
 
 @Service
-@Transactional
 public class AccountService {
     AccountRepository accountRepository;
 
@@ -20,35 +17,14 @@ public class AccountService {
 
     /**
      * @param transient account
-     * @return persisted account
+     * @return saved account
      */
-    public Account persistAccount(Account account) {
-        return accountRepository.save(account);
-    }
-
-    /**
-     * @return all account entities
-     */
-    public List<Account> getAllAccounts() {
-        return accountRepository.findAll();
-    }
-
-    /**
-     * @param account entity's id
-     * @return account entity
-     */
-    public Account getAccountById(long id) {
-        Optional<Account> optionalAccount = accountRepository.findById(id);
-
-        if (optionalAccount.isPresent()) {
-            return optionalAccount.get();
-        } else {
-            return null;
+    public Account save(Account account) {
+        if (accountRepository.findAccountByUsername(account.getUsername()) == null) {
+            accountRepository.save(account);
+            return account;
         }
-    }
-
-    public void deleteAccount(long id) {
-        accountRepository.deleteById(id);
+        return null;
     }
 
     /**
@@ -56,7 +32,7 @@ public class AccountService {
      * @param replacement: account entity with updated field values
      * @return the updated account entity
      */
-    public Account updateAccount(long id, Account replacement) {
+    public Account update(Integer id, Account replacement) {
         // findById returns a type Optional<Account>. This helps the developer avoid null pointer
         // exceptions. We can use the method .get() to convert an Optional<Account> to Account.
         Optional<Account> optionalAccount = accountRepository.findById(id);
@@ -69,5 +45,20 @@ public class AccountService {
         } else {
             return null;
         }
+    }
+
+    public void delete(Integer id) {
+        accountRepository.deleteById(id);
+    }
+
+    /**
+     * @param transient account
+     * @return found account entity
+     */
+    public Account login(Account account) {
+        return accountRepository.findAccountByUsernameAndPassword(
+            account.getUsername(),
+            account.getPassword()
+        );
     }
 }
